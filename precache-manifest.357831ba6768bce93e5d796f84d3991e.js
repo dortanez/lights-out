fc704006e055a391ee49bacbb21f4b4d7b6df278
Danisa Ortanez <dortanez@yahoo.com>
2020-12-22 21:50:32 -0800

    Updates

---
+++ precache-manifest.357831ba6768bce93e5d796f84d3991e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95033f5c24cf1d61ddd8f1b451cc411b",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "077c9545a17937bc43e5",
+    "url": "/lights-out/static/css/main.03943d8f.chunk.css"
+  },
+  {
+    "revision": "173261948641a3a554d8",
+    "url": "/lights-out/static/js/2.7b9ea367.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lights-out/static/js/2.7b9ea367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077c9545a17937bc43e5",
+    "url": "/lights-out/static/js/main.28b8809a.chunk.js"
+  },
+  {
+    "revision": "9b3223a1384e8d3b1c3e",
+    "url": "/lights-out/static/js/runtime-main.d3554513.js"
+  }
+]);
